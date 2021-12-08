4414b134bbce3ad1d94ce7b77666c81b240d5527
AntonAroche <anton.aroche@gmail.com>
2021-12-07 23:46:26 -0500

    Add footer and more items

--- frontend/ecommerce-site/src/app/app.module.ts
+++ frontend/ecommerce-site/src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreitemComponent } from './storeitem/storeitem.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,10 @@ import { StoreitemComponent } from './storeitem/storeitem.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
